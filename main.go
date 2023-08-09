73df1c713ebb1af9b82f92b35d73e3f170c59429
Plinio Nascimento <plinio.gsnascimento@gmail.com>
2023-08-08 23:04:34 -0300

    [feat] Handle methods on route

--- main.go
+++ main.go
@@ -22,7 +22,7 @@ func main() {
 
 	router := http.NewServeMux()
 	router.HandleFunc("/", handleRoot)
-	router.HandleFunc("/hello", handleRoot)
+	router.HandleFunc("/hello", handleHello)
 
 	http.Handle(
 		"/metrics",
@@ -31,7 +31,10 @@ func main() {
 				registry,
 				promhttp.HandlerOpts{}),
 			))
+
+	// Add middlewares
 	http.Handle("/", middleware.New(registry, nil).WrapHandler("/", router))
+	http.Handle("/hello", middleware.New(registry, nil).WrapHandler("/hello", router))
 	err := http.ListenAndServe(":8080", nil)
 
 	if errors.Is(err, http.ErrServerClosed) {
@@ -43,6 +46,20 @@ func main() {
 }
 
 func handleRoot(w http.ResponseWriter, r *http.Request) {
-	log.Println("Request received")
-	io.WriteString(w, "OK")
+	log.Println("Not found")
+	w.WriteHeader(http.StatusNotFound)
+	io.WriteString(w, "Not found")
+}
+
+func handleHello(w http.ResponseWriter, r *http.Request) {
+
+	switch r.Method {
+	case http.MethodGet:
+		log.Println("Request received on hello")
+		io.WriteString(w, "Hello there!")
+	default:
+		log.Println("Request received on hello")
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	}
+
 }
